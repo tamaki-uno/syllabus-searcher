ce92b24087f719a76dca2b92c0eab8521fe9bda0
tamaki uno <t.uno@keio.jp>
2024-10-29 12:23:06 +0900

    Refactor scraper.js and update scrape function make it not to use selector but text

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,6 +1,7 @@
 // インポート 
 const axios = require('axios'); // HTTPクライアント
 const cheerio = require('cheerio'); // HTML解析
+const { log } = require('console');
 const fs = require('fs'); // ファイル操作
 
 const HOST_URL = 'https://syllabus.sfc.keio.ac.jp'; // ホスト名のURL
@@ -58,6 +59,8 @@ async function scrape(url, selectors, type='text', save=false, urlsave=false) {
 
 // 複数のURLをスクレイピングする関数
 async function scrapePages(urls, selectors, type='text', save=false, form='connect', urlsave=false) {
+    console.log(`#scrape pages. estimated time: ${urls.length}sec`);
+    let timeRemain = urls.length;
     let datas;
     switch (form) {
         case 'connect':
@@ -71,6 +74,7 @@ async function scrapePages(urls, selectors, type='text', save=false, form='conne
             break;
     }
     for (const url of urls) {
+        if (timeRemain % 10 === 0) console.log(`time remain: ${timeRemain}sec`); // 10秒ごとに残り時間を表示
         const data = await scrape(url, selectors, type, save, urlsave); // スクレイピング実行
         switch (form) {
             case 'connect':
@@ -84,6 +88,7 @@ async function scrapePages(urls, selectors, type='text', save=false, form='conne
                 break;
         }
         await new Promise(resolve => setTimeout(resolve, 1000)); // 1秒待機
+        timeRemain--;
     }
     return datas;
 }
@@ -117,7 +122,7 @@ function searchURIGenerator(page='', title='', year='', semester='', sub_semeste
 
 // 複数の検索結果ページをスクレイピングしてURLを取得する関数
 async function scrapePagesToGetUrls(pageNums, title='', year='', semester='', sub_semester='', teacher_name='', day_codes='', time_codes='', departments='', sfc_guide_title='', languages='', summary='', locations='', styles=''){
-    console.log('#scrape page to get urls')
+    console.log(`#scrape page to get urls. this may take maximum ${pageNums*25}secs.`);
     const searchURLs = [];
     // ページ数分のURLを生成
     for (let page = 1; page <= pageNums; page++) {
@@ -179,76 +184,79 @@ async function scrapeSyllabus(pageNums, title='', year='', semester='', sub_seme
     //     // 以下詳細
     //     '講義概要': 'body > div.main > div > div:nth-child(3) > dl > dd > p'
     // };
-    const selectors = {
-        'body > div.main > div > h2 > span.title-label': 'body > div.main > div > h2 > span.title', // 科目名
-        // `${selecterKB}h2 > span.title-label`: `${selecterKB}h2 > span.title`, // 科目名
 
-        'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dt:nth-child(1)': 'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dd:nth-child(2)', // 学部・研究科
-        'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dt:nth-child(3)': 'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dd:nth-child(4)', // 登録番号
+    // const selectors = {
+    //     'body > div.main > div > h2 > span.title-label': 'body > div.main > div > h2 > span.title', // 科目名
+    //     // `${selecterKB}h2 > span.title-label`: `${selecterKB}h2 > span.title`, // 科目名
 
-    };
+    //     'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dt:nth-child(1)': 'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dd:nth-child(2)', // 学部・研究科
+    //     'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dt:nth-child(3)': 'body > div.main > div > div:nth-child(2) > div.class-info > div:nth-child(1) > dl:nth-child(1) > dd:nth-child(4)', // 登録番号
+
+    // };
 
 
     // スクレイピングの実行
     // const data = await scrapePages(urls, selectors, 'text', true, 'array', true);
-    const data = await scrapePages(urls, selectors, 'text', false, 'array', true);
-    // const data = await scrapePages(urls, selectors, 'both', false, 'array', true);
-
-    return data;
+    
+    const selectors = {body: 'body'};
+    const dataArray = await scrapePages(urls, selectors, 'text', false, 'array', true);
+    // console.log('data:', data);
+    const results = [];
+    for (data of dataArray) {
+        // console.log('data:', data);
+        if (data['body'] === undefined) return 'couldnt find body from:'+data[URL] ; // データが取得できなかった場合はスキップ
+        
+        const shapedData = data['body'].replaceAll(' \n', '').replaceAll('  ', ''); // 余分な改行とスペースを削除
+        const formatedData = shapedData.split('\n'); // 改行で分割
+        // console.log('formatedData:', formatedData);
+        const result = {
+            'URL': data['URL'], // URL
+            '科目名': formatedData[8].replace('科目名', '').replace('Subject', '') // 科目名
+        };
+        for (let i = 9; i < formatedData.length; i += 2) {
+            result[formatedData[i]] = formatedData[i + 1]; // その他項目
+        }
+        // const data = await scrapePages(urls, selectors, 'both', false, 'array', true);
+        // console.log('result:', result);
+        results.push(result);
+    }
+    console.log(`scraped ${results.length} courses`);
+    // return data;
+    return results;
 }
 
 // メイン関数
 async function main(){
-    const pageNums = 10;
+    console.log('start'); // 開始メッセージ
+    const pageNums = 100;
     const title = '';
     const year = '2024';
     const semester = 'fall';
+
+    const now = new Date(); // 現在時刻を取得
+    // const filename = `data\\data_${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}_${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.json`; // ファイル名 (data_年/月/日_時:分:秒.json)
+    const filename = `data\\data_${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}_${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}.json`; // ファイル名 (data_年/月/日_時:分:秒.json)
+    console.log('filename:', filename); // ファイル名を表示
 
     // // 引数から取得
     // // process.argv[0]はNode.jsの実行プロセスのパス、process.argv[1]は実行したスクリプトファイルのパス
     // // process.argv[2]以降が引数
     // const pageNums, title, year, semester, sub_semester, teacher_name, day_codes, time_codes, departments, sfc_guide_title, languages, summary, locations, styles = process.argv.slice(2);
 
-    // const url = searchURIGenerator(10, '', '2024', 'fall');
-    const url = 'https://syllabus.sfc.keio.ac.jp/courses/2024_38384?locale=ja';
-    let result = await scrape(url, {body: 'body'}, 'text', false, false);
-    result = result['body'];
-    // result = result.replace('\n', '');
-    // result = result.replace('\t', '');
-    result = result.replaceAll(' \n', '');
-    result = result.replaceAll(' ', '');
-    // result = result.replace('　', '');
-    // result = result.replace('\r', '');
-    // result = result.replace('\r\n', '');
-    // let resultArray = result.split('\n');
-    // for (let i = 0; i < resultArray.length; i++) {
-    //     resultArray[i] = resultArray[i].replace(' ', '');
+    const data = await scrapeSyllabus(pageNums, title, year, semester); // スクレイピング実行
+    // const data = {
+    //     'key': 'value',
+    //     'key2': 'value2'
     // }
-    // let resultArrayCode = resultArray.map((result) => {
-    //     // return result.charCodeAt(0);
-    //     // return ' ' in result;
-    //     return result.includes(' ') ? null : result;
-    // })
-    // for (let result in resultArray) {
-    //     if (resultArray[result] != '' || resultArray[result] != ' ' || resultArray[result] != '　' || resultArray[result] != '\r' || resultArray[result] != '\r\n'){
-    //         resultArray2.push(resultArray[result]);
-    //     }
-    // }
-
-    // let resultObj = {};
-
-    // for (let i = 0; i < resultArray.length; i++) {
-    //     resultObj[resultArrayCode[i]] = resultArray[i];
-    // }
-
-    console.log(result);
-    // console.log(resultArray);
-    // console.log(resultArrayCode);
-    // console.log(resultObj);
-
-    // const data = await scrapeSyllabus(pageNums, title, year, semester); // スクレイピング実行
-    // fs.writeFileSync('data.json', JSON.stringify(data, null, 4)); // ファイルに保存
     // console.log('data:', data);
+    // fs.writeFile()
+    // fs.writeFileSync('data.json', JSON.stringify(data, null, 4)); // ファイルに保存
+    // fs.writeFileSync(filename, JSON.stringify(data, null, 4)); // ファイルに保存
+    fs.writeFile(filename, JSON.stringify(data, null, 4), (err) => {
+        if (err) throw err;
+    }); // ファイルに保存
+
+
     console.log('done'); // 完了メッセージ
 }
 
