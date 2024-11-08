4661b6bff7d2000b2bf8626d6a1fe1c99160f1eb
tamaki UNO <t.uno@keio.jp>
2024-11-08 09:46:20 +0900

    スクレイピング関数に待機時間を追加し、英語版のURL生成機能を実装。データ取得処理を整理し、不要なコードを削除。

--- scraper/simple_scraper.js
+++ scraper/simple_scraper.js
@@ -7,9 +7,10 @@ const fs = require('fs'); // ファイル操作
 const HOST_URL = 'https://syllabus.sfc.keio.ac.jp'; // ホスト名のURL
 
 // スクレイピングする関数
-async function scrape(url, selectors, num=1) {
+async function scrape(url, selectors, num=1, delay=1000) {
     try {
         console.log(`#scrape ${url}`); // 開始メッセージ
+        await new Promise(resolve => setTimeout(resolve, delay)); // 1秒待機
         const response = await axios.get(url); // リクエストを送信
         const html = response.data; // レスポンスのHTMLを取得
         const $ = cheerio.load(html); // cheerioを使ってHTMLを解析
@@ -53,7 +54,7 @@ async function scrapePages(urls, selectors, num=1) {
         if (timeRemain % 10 === 0) console.log(`time remain: ${timeRemain}sec`); // 10秒ごとに残り時間を表示
         const data = await scrape(url, selectors, num); // スクレイピング実行
         datas.push(...data); // データを結合
-        await new Promise(resolve => setTimeout(resolve, 1000)); // 1秒待機
+        // await new Promise(resolve => setTimeout(resolve, 1000)); // 1秒待機
         timeRemain--;
     }
     return datas;
@@ -63,27 +64,34 @@ async function scrapePages(urls, selectors, num=1) {
 function searchURIGenerator(page='', title='', year='', semester='', sub_semester='', teacher_name='', day_codes='', time_codes='', departments='', sfc_guide_title='', languages='', summary='', locations='', styles=''){
     // URLのクエリパラメータを変更して検索結果ページを取得 &つなぎでパラメータを追加 順番は関係ないかもしれない
     let uri = `${HOST_URL}/courses`; // ホストURLを追加 これはURL 以下からURI
+    let enUri = `${HOST_URL}/courses`; // 英語版のURL
 
     // クエリパラメータを追加 (空の場合は追加しない)
     uri += '?locale=ja'; // 言語
-    if (page) (page === '1') ? uri += '' : uri += `&page=${page}`; // ページ番号 (1ページ目の場合は何も追加しない)
-    if (title) uri += `&search[title]=${title}`; // タイトル
-    if (year) uri += `&search[year]=${year}`; // 年度
-    if (semester) uri += `&search[semester]=${semester}`; // 学期
-    if (sub_semester) uri += `&search[sub_semester]=${sub_semester}`; // 前半/後半
-    if (teacher_name) uri += `&search[teacher_name]=${teacher_name}`; // 教員名
-    if (day_codes) uri += `&search[day_codes][]=${day_codes}`; // 曜日
-    if (time_codes) uri += `&search[time_codes][]=${time_codes}`; // 時限
-    if (departments) uri += `&search[departments][]=${departments}`; // 学部
-    if (sfc_guide_title) uri += `&search[sfc_guide_title]=${sfc_guide_title}`; // SFCガイド
-    if (languages) uri += `&search[languages][]=${languages}`; // 言語
-    if (summary) uri += `&search[summary]=${summary}`; // 概要
-    if (locations) uri += `&search[locations][]=${locations}`; // 場所
-    if (styles) uri += `&search[styles][]=${styles}`; // スタイル
-    // uri += '&button='; // ボタン
+    enUri += '?locale=en'; // 英語版の言語
+    let queryParameters = ''; // 検索クエリを初期化
+    if (page) (page === '1') ? queryParameters += '' : queryParameters += `&page=${page}`; // ページ番号 (1ページ目の場合は何も追加しない)
+    if (title) queryParameters += `&search[title]=${title}`; // タイトル
+    if (year) queryParameters += `&search[year]=${year}`; // 年度
+    if (semester) queryParameters += `&search[semester]=${semester}`; // 学期
+    if (sub_semester) queryParameters += `&search[sub_semester]=${sub_semester}`; // 前半/後半
+    if (teacher_name) queryParameters += `&search[teacher_name]=${teacher_name}`; // 教員名
+    if (day_codes) queryParameters += `&search[day_codes][]=${day_codes}`; // 曜日
+    if (time_codes) queryParameters += `&search[time_codes][]=${time_codes}`; // 時限
+    if (departments) queryParameters += `&search[departments][]=${departments}`; // 学部
+    if (sfc_guide_title) queryParameters += `&search[sfc_guide_title]=${sfc_guide_title}`; // SFCガイド
+    if (languages) queryParameters += `&search[languages][]=${languages}`; // 言語
+    if (summary) queryParameters += `&search[summary]=${summary}`; // 概要
+    if (locations) queryParameters += `&search[locations][]=${locations}`; // 場所
+    if (styles) queryParameters += `&search[styles][]=${styles}`; // スタイル
+    // queryParameters += '&button='; // ボタン
 
-    const url = encodeURI(uri); // URIエンコードしてURIを生成 (日本語などの文字列をエンコード)
-    return url;
+    // const url = encodeURI(uri); // URIエンコードしてURIを生成 (日本語などの文字列をエンコード)
+    // const enUrl = encodeURI(enUri); // 英語版のURIを生成
+    const url = uri + queryParameters; // URIを生成
+    const enUrl = enUri + queryParameters; // 英語版のURIを生成
+
+    return url, enUrl; // URLを返す
 }
 
 async function searchURIsGenerator(title='', year='', semester='', sub_semester='', teacher_name='', day_codes='', time_codes='', departments='', sfc_guide_title='', languages='', summary='', locations='', styles=''){
@@ -137,11 +145,13 @@ async function scrapeSyllabusSimply(title='', year='', semester='', sub_semester
         'Course Summary': 'li:nth-child({num}) > div.hidden-field > div.outline',
         'URL': 'li:nth-child({num}) > div.detail-btn-wrapper > a'
     }
-
+    const num = 25; // 1ページあたりの取得タイル数
     // スクレイピング実行
-    const dataArray = await scrapePages(searchURLs, selectors, searchURLs.length); // スクレイピング実行
+    const dataArray = await scrapePages(searchURLs, selectors, num); // スクレイピング実行
     const nullDeletedDataArray = dataArray.filter((data) => data['Subject'] !== undefined); // Subjectが空のデータを削除 
+    // console.log(`***nullDeletedDataArray: ${nullDeletedDataArray}***`); // データ
     const results = nullDeletedDataArray.map((data) => {
+        // console.log('data:', data); // データを表示
         // 加工ゾーン
         for (const key in data) {
             data[key] = data[key].replaceAll('  ', ''); // ダブルスペースを削除
@@ -162,10 +172,13 @@ async function scrapeSyllabusSimply(title='', year='', semester='', sub_semester
                     }
                     break;
                 case 'Lecturer Name': // 教員名を分割
-                    data[key] = data[key].split(' \n').filter((name) => name !== ''); // 教員名を分割&空の要素を削除
+                    data[key] = data[key].split('\n').filter((name) => name !== ''); // 教員名を分割&空の要素を削除
                     break;
                 case 'URL': // URLを加工
                     data[key] = HOST_URL + data[key].replace('?locale=ja', ''); // URLのlocale=jaを削除
+                    break;
+                case 'Course Summary': // コース概要を加工
+                    data[key] = data[key].replace('\n', ''); // 先頭の改行を削除
                     break;
                 default:
                     break;
@@ -173,7 +186,7 @@ async function scrapeSyllabusSimply(title='', year='', semester='', sub_semester
         }
         return data;
     });
-
+    console.log(`@got ${results.length} data`); // データ数を表示-
     return results;
 }
 
@@ -183,6 +196,12 @@ async function main(){
     const title = '';
     const year = '2024';
     const semester = 'fall';
+    const sub_semester = '';
+    const teacher_name = '';
+    const day_codes = '';
+    const time_codes = '';
+    const departments = '';
+    const sfc_guide_title = '23/11/2014/2.Fundamental Subjects - Introductory Subjects';
 
     const now = new Date(); // 現在時刻を取得
     const filepath = `data\\data_${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}_${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}_${year}_${semester}.json`; // ファイル名 (data_年-月-日_時-分-秒_ページ数_タイトル_年度_学期.json)
@@ -193,10 +212,11 @@ async function main(){
     // // process.argv[2]以降が引数
     // const pageNums, title, year, semester, sub_semester, teacher_name, day_codes, time_codes, departments, sfc_guide_title, languages, summary, locations, styles = process.argv.slice(2);
 
-    const data = await scrapeSyllabusSimply(title, year, semester); // スクレイピング実行
+    // スクレイピング実行
+    const data = await scrapeSyllabusSimply(title, year, semester, sub_semester, teacher_name, day_codes, time_codes, departments, sfc_guide_title); // スクレイピング実行
 
-    await fs.writeFileSync(filepath, JSON.stringify(data, null, 4)); // ファイルに保存
-    // console.log('data:', data); // データを表示
+    // await fs.writeFileSync(filepath, JSON.stringify(data, null, 4)); // ファイルに保存
+    console.log('data:', data); // データを表示
 
     console.log('done'); // 完了メッセージ
 }
